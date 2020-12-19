98aeffb04a0b3e8150e6f91e61d6acc5a9bfee98
virajdoshi <44646580+virajdoshi@users.noreply.github.com>
2020-12-19 15:10:02 +0530

    removed unnecessary codes

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 const mongoose = require('mongoose');
 var methodOverride = require("method-override")
 
-var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -32,7 +31,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler

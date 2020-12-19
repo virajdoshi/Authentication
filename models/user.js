5db971ec883a678d1f0f278f60c9c29ed65d0479
virajdoshi <44646580+virajdoshi@users.noreply.github.com>
2020-12-19 15:08:14 +0530

    OTP verification for signup

--- models/user.js
+++ models/user.js
@@ -4,11 +4,13 @@ const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
 var UserSchema = new mongoose.Schema({
-    phone: {type: String, unique: true, required: [true, "can't be blank"], match: [/^[0-9]+$/, 'is invalid'], index: true},
-    email: {type: String, unique: true, required: [true, "can't be blank"], match: [/\S+@\S+\.\S+/, 'is invalid'], index: true},
-    first_name: {type: String, required: [true, "can't be blank"]},
-    last_name: {type: String, required: [true, "can't be blank"]},
-    password: {type: String, required: [true, "can't be blank"]}
+    phone: {type: String, unique: true,  match: [/^[0-9]+$/, 'is invalid'], index: true},
+    email: {type: String, unique: true, index: true},
+    first_name: {type: String},
+    last_name: {type: String},
+    password: {type: String},
+    otp: {type: Number},
+    isVerified: {type: Boolean}
 }, {timestamps: true});
 
 UserSchema.plugin(uniqueValidator, {message: 'is already taken.'});
@@ -18,26 +20,73 @@ const userModel = mongoose.model('User', UserSchema);
 module.exports = {
     register: async function(data){
         return new Promise(function(resolve) {
-            userModel.findOne({phone: data.user.phone}, function(err, foundUser){
-                bcrypt.hash(data.user.password, saltRounds, (err, hash) => {
-                    const newUser = new userModel({
-                        phone: data.user.phone,
-                        email: data.user.email,
-                        first_name: data.user.first_name,
-                        last_name: data.user.last_name,
-                        password: hash
-                    })
-                    newUser.save(function(err){
-                        if(err){
-                            return resolve("Message: Registration Failed beacause your phone number already exist");
+            userModel.exists({phone: data.user.phone, email: data.user.email}, function(err, results){
+                if(results === true){
+                    resolve("Message: Registration Failed beacause you are already registered")
+                }
+                else{
+                    userModel.exists({phone: data.user.phone, isVerified: true}, function(err, results){
+                        if(results === true){
+                            bcrypt.hash(data.user.password, saltRounds, (err, hash) => {    
+                                userModel.updateOne({phone: data.user.phone},{$set: {
+                                    email: data.user.email,
+                                    first_name: data.user.first_name,
+                                    last_name: data.user.last_name,
+                                    password: hash
+                                }}, function(err, results){
+                                    if(err){
+                                        resolve("Message: This email is already registered");
+                                    }
+                                    else
+                                        resolve("Message: Registration successfully completed now you can login.")
+                                })
+                            })
                         }
                         else{
-                            resolve("Message: Registration Successfull");
+                            return resolve("Message: Before you registered your personal details first verify your phone number.");
                         }
                     })
-                })
+                }
+            })
+        })
+    },
+
+    registerPhone: async function(data){
+        return new Promise(function(resolve) {
+            userModel.exists({phone: data.user.phone}, function(err, results){
+                if(results === false){
+                    var otp = Math.floor(100000 + Math.random() * 900000);
+                    const newUser = new userModel({
+                        phone: data.user.phone,
+                        otp: otp
+                    })
+                    newUser.save(function(err){
+                        if(err)
+                            console.log(err)
+                        else
+                            resolve("Message: Your otp is "+ otp + ". Now verify this otp at /verify-otp");
+                    })
+                }
+                else{
+                    resolve("Message: Registration failed because your are already registered your phone number");
+                }
+            })
+        })
+    },
+
+    verifyOTP: async function(data){
+        return new Promise(function(resolve){
+            userModel.exists({phone: data.user.phone, otp: data.user.otp}, function(err, results){
+                if(results === false){
+                    resolve("Message: Before you verify first register your phone at /register-phone or check your otp properly.");
+                }
+                else{
+                    userModel.updateOne({phone: data.user.phone}, {$set: {isVerified: true}}, function(err, results){
+                        console.log(results);
+                    })
+                    resolve("Message: Your phone number is verified now you must register your personal details at /register")
+                }
             })
         })
     }
 }
-
